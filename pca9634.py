be5c8cdee8d59db267dcdc9b585a762cbf9cceec
Maxime Guillemette <maxime_glmt@hotmail.ca>
2022-06-05 21:00:14 -0400

    updated pca9634, works as intended with my pcb

--- pca9634.py
+++ pca9634.py
@@ -8,7 +8,9 @@ Second byte = Control register:
 """
 
 
-# Register definitions
+BRIGHTNESS_GREEN  = 0x80
+BRIGHTNESS_YELLOW = 0xff
+BRIGHTNESS_RED    = 0xff
 
 
 class PCA9634:
@@ -33,8 +35,14 @@ class PCA9634:
     ALLCALLADR = 0x11
     # flags
     AUTOINC    = 0x80
+    
+    # LED output state control values
+    LED_OFF = 0
+    LED_ON  = 1
+    LED_PWM = 2
+    LED_GRP = 3
 
-    def __init__(self, i2c, address: int, brightness: int=0x5):
+    def __init__(self, i2c, address: int, brightness: int=0xf):
         self.i2c = i2c
         self.address = address
         self.configure()
@@ -43,24 +51,35 @@ class PCA9634:
 
     def i2c_write_reg(self, reg, data):
         return self.i2c.writeto(self.address, bytes([reg, data]))
-
+    
     def i2c_write_autoincrement(self, reg, data):
         return self.i2c.writeto(self.address, bytes([reg | PCA9634.AUTOINC] + list(data)))
 
     def configure(self):
-        self.i2c_write_reg(PCA9634.MODE1, 0x00)
-        self.i2c_write_reg(PCA9634.MODE2, 0x05)
+        r = self.i2c_write_reg(PCA9634.MODE1, 0x00)
+        print(r)
+        r = self.i2c_write_reg(PCA9634.MODE2, 0x05)
+        print(r)
 
     def set_brightness(self, brightness: int):
         assert 0 <= brightness <= 0xff, f'brightness must be 8 bit value'
         self.brightness = brightness
         self.i2c_write_reg(PCA9634.GRPPWM, self.brightness)
+        self.i2c_write_reg(PCA9634.GRPFREQ, self.brightness)
+        self.i2c_write_autoincrement(PCA9634.PWM0, [BRIGHTNESS_GREEN,
+                                                    BRIGHTNESS_GREEN,
+                                                    BRIGHTNESS_GREEN,
+                                                    BRIGHTNESS_GREEN,
+                                                    BRIGHTNESS_YELLOW,
+                                                    BRIGHTNESS_YELLOW,
+                                                    BRIGHTNESS_RED,
+                                                    BRIGHTNESS_RED])
 
-    def set_leds_states(self, states: list=[0]*8):
+    def set_leds_states(self, states: list=[1]*8):
         x = 0
 
         for i, state in enumerate(states):
-            z = 3 if state else 0
+            z = PCA9634.LED_GRP if state else PCA9634.LED_OFF
             x |= z << (2 * i)
 
         return self.i2c_write_autoincrement(PCA9634.LEDOUT0, x.to_bytes(2, 'little'))
